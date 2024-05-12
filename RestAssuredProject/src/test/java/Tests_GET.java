47dd37bdeab1d8d86920794ab85714d867f419b5
Aleksandros Vuros <aleksandrosvouros@gmail.com>
2024-05-12 13:31:35 +0300

    Adding post put delete tests

--- RestAssuredProject/src/test/java/Tests_GET.java
+++ RestAssuredProject/src/test/java/Tests_GET.java
@@ -11,9 +11,8 @@ public class Tests_GET {
 			get("https://reqres.in/api/users?page=2").
 		then().
 			statusCode(200).
-			body("data.id[1]", equalTo(8)).
+			body("data.id[0]", equalTo(7)).
 			body("data.first_name", hasItems("Michael", "Lindsay")).
 			log().all();
 	}
-
 }
---
+++ RestAssuredProject/src/test/java/Tests_POST.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+
+public class Tests_POST {
+	
+	@Test
+	public void test_1_post() {
+		JSONObject request = new JSONObject();
+		request.put("name", "Alex");
+		request.put("jobs", "QA Automation");
+
+		System.out.println(request.toJSONString());
+
+		given().
+			header("Content-Type", "application/json").
+			contentType(ContentType.JSON).accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			post("https://reqres.in/api/users").
+		then().
+			statusCode(201);
+	}
+
+	@Test
+	public void test_2_put() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "Aleksandros");
+		map.put("jobs", "QA Automation");
+
+		JSONObject request = new JSONObject(map);
+
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Content-Type", "application/json").
+			contentType(ContentType.JSON).accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			put("https://reqres.in/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+	}
+	
+	@Test
+	public void test_3_patch() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "Aleksandros");
+		map.put("jobs", "QA Automation");
+
+		JSONObject request = new JSONObject(map);
+
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Content-Type", "application/json").
+			contentType(ContentType.JSON).accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("https://reqres.in/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+	}
+	
+	@Test
+	public void test_4_delete() {
+		given().
+			delete("https://reqres.in/api/users/2").
+		then().
+			statusCode(204).
+			log().all();
+	}
+
+}
