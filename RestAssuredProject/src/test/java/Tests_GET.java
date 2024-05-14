4f10676cac2ce876e48558dbfab154bca3e173c1
Aleksandros Vuros <aleksandrosvouros@gmail.com>
2024-05-14 11:56:47 +0300

    Adding new tests

--- RestAssuredProject/src/test/java/Tests_GET.java
+++ RestAssuredProject/src/test/java/Tests_GET.java
@@ -3,6 +3,8 @@ import static org.hamcrest.Matchers.*;
 
 import org.testng.annotations.Test;
 
+import io.restassured.http.ContentType;
+
 public class Tests_GET {
 
 	@Test
@@ -15,4 +17,67 @@ public class Tests_GET {
 			body("data.first_name", hasItems("Michael", "Lindsay")).
 			log().all();
 	}
+
+    @Test
+    public void testCreateUserSuccess() {
+        String requestBody = """
+            {
+                "name": "morpheus",
+                "job": "leader",
+                "age": 21
+            }
+            """;
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+        .when()
+            .post("https://reqres.in/api/users") // Replace with your endpoint
+        .then()
+            .statusCode(201)
+            .body("name", equalTo("morpheus"))
+            .body("job", equalTo("leader"))
+            .body("id", notNullValue())
+            .body("createdAt", notNullValue());
+    }
+
+    @Test
+    public void testCreateUserWithoutAge() {
+        String requestBody = """
+            {
+                "name": "morpheus",
+                "job": "leader"
+            }
+            """;
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+        .when()
+            .post("https://reqres.in/api/users")
+        .then()
+            .statusCode(201)
+            .body("name", equalTo("morpheus"))
+            .body("job", equalTo("leader"))
+            .body("id", notNullValue())
+            .body("createdAt", notNullValue());
+    }
+
+    @Test
+    public void testCreateUserMissingJob() {
+        String requestBody = """
+            {
+                "name": "morpheus",
+                "age": 21
+            }
+            """;
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+        .when()
+            .post("https://reqres.in/api/users")
+        .then()
+            .statusCode(400); // Assuming 400 is returned for missing required fields
+    }
 }
